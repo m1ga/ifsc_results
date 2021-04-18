b663088957fbe4b9bce716be016990d72a870fb7
m1ga <migamiga@web.de>
2021-04-18 13:20:30 +0200

    init

---
+++ README.md
@@ -0,0 +1,31 @@
+# IFSC Climbing Results
+Displaying IFSC Climbing results with nodejs inside a console
+<img src="img/screen1.jpg"/>
+
+## How to
+
+```bash
+git clone https://github.com/m1ga/ifsc_results
+cd ifsc_results
+npm i
+node index.js
+```
+
+The screen will refresh every minute with new results.
+
+## Change event
+
+To get the event ID you currently have to go to:
+https://www.ifsc-climbing.org/index.php/world-competition/calendar
+get the `event_id` (e.g. `1187`) and open this URL with your event id:
+https://components.ifsc-climbing.org/results-api.php?api=overall_r_result_complete&event_id=1187&category_id=3
+_(change `1187` to your event ID)_
+
+From there you grab the ID from `categry_rounds` you would like to follow:
+<img src="img/screen2.png"/>
+Here it is `6376`
+
+## TODO
+* extend the script so you can select the event and category without using the page!
+* auto-select current event
+* better UI
---
+++ index.js
@@ -0,0 +1,49 @@
+const category_id = 6377
+const urlCompetition = "https://components.ifsc-climbing.org/results-api.php?api=event_full_results&result_url=/api/v1/category_rounds/" + category_id + "/results";
+const fetch = require('node-fetch');
+const colors = require('colors');
+let settings = {
+	method: "Get"
+};
+
+function getResults(url) {
+	fetch(url, settings)
+		.then(res => res.json())
+		.then((json) => {
+			console.clear();
+			console.log("  " + json.discipline + " " + json.category + " " + json.round);
+			if (json.ranking) {
+				json.ranking.forEach((athelete, i) => {
+					let name = athelete.firstname + " " + athelete.lastname;
+					let route = "";
+					let topCount = 0;
+					let zoneCount = 0;
+					if (athelete.active) name = colors.yellow(name);
+					if (athelete.ascents) {
+						athelete.ascents.forEach((ascents, i) => {
+							let _tries = ascents.top_tries || 0;
+							let _zones = ascents.zone_tries || 0;
+							if (ascents.zone) zoneCount++;
+							if (ascents.top) topCount++;
+
+							let currentRoute = "\n    | " + ascents.route_name + " Zone: " + ((ascents.zone) ? colors.green("yes") : "no") + "\ttries: " + _zones + "\t| Top: " + ((ascents.top) ? colors.green("yes") : "no") + "\ttries: " + _tries;
+							if (ascents.status == "pending") {
+								currentRoute = colors.grey(currentRoute);
+							}
+							route += currentRoute;
+						});
+					}
+
+					console.log("\n  " + name + " | T:" + topCount + " | Z:" + zoneCount + route);
+				});
+				console.log("-----------");
+			} else {
+				console.log("\n  - no ranking yet -");
+			}
+		});
+}
+
+getResults(urlCompetition);
+setInterval(function() {
+	getResults(urlCompetition);
+}, 60 * 1000);
